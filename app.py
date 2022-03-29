edbcb0ddff41e55f2cf7712a0ba0987cf9a4d873
RimmalapudiRajesh <94752398+RimmalapudiRajesh@users.noreply.github.com>
2022-03-29 19:58:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 
 cluster = MongoClient("mongodb+srv://raghu:raghu@cluster0.guea2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
 db = cluster["bakery"]
 users = db["users"]
 orders = db["orders"]
@@ -20,8 +21,8 @@ def reply():
     res = MessagingResponse()
     user = users.find_one({"number": number})
     if bool(user) == False:
-        msg = res.message("Hi, thanks for contacting The Red Velvet.\nYou can choose from one of the options below: "
-                    "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order snacks \n 3️⃣ To know our working hours \n 4️⃣"
+        msg = res.message("Hi, thanks for contacting Rajesh Restaurant.\nYou can choose from one of the options below: "
+                    "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order food \n 3️⃣ To know our working hours \n 4️⃣"
                     "To get our address")
         users.insert_one({"number": number, "status": "main", "messages": []})
     elif user["status"] == "main":
@@ -33,18 +34,18 @@ def reply():
 
         if option == 1:
             res.message(
-                "You can contact us through phone or e-mail.\n\nPhone: 9392741313 \n*E-mail* :rajesh12345@gmail.com")
+                "You can contact us through phone or e-mail.\n\nPhone: 9392741313 \n E-mail :rajesh12345@gmail.com")
         elif option == 2:
-            res.message("You have entered *ordering mode*.")
+            res.message("You have entered ordering mode.")
             users.update_one(
-                {"number": number}, {"$set": {"status": "ordering"}})
-            res.message("You can select one of the following cakes to order: \n\n1️⃣ Red Velvet  \n2️⃣ Dark Forest \n3️⃣ Ice Cream Cake"
-                "\n4️⃣ Plum Cake \n5️⃣ Sponge Cake \n6️⃣ Genoise Cake \n7️⃣ Angel Cake \n8️⃣ Carrot Cake \n9️⃣ Fruit Cake  \n0️⃣ Go Back")
+                {"number": number}, {"$set": {"status": "ordering" ""}})
+            res.message("You can select one of the following food to order: \n\n1️⃣ Veg Biryani  \n2️⃣ Chicken Biryani \n3️⃣ Chicken Spec.Biryani"
+                "\n4️⃣ Mutton Biryani \n5️⃣ Chicken Wings \n6️⃣ Paneer Biryani \n7️⃣ Chicken Curry \n8️⃣ Mutton Curry \n9️⃣ Leg pieces  \n0️⃣ Go Back")
         elif option == 3:
-            res.message("We work from *9 a.m. to 5 p.m*.")
+            res.message("We work from 9 a.m. to 5 p.m.")
 
         elif option == 4:
-            res.message("We have multiple stores across the city. Our main center is at *4/54, guntur")
+            res.message("We have multiple stores across the city. Our main center is at 1/147, guntur")
         else:
             res.message("Please enter a valid response")
     elif user["status"] == "ordering":
@@ -57,11 +58,11 @@ def reply():
             users.update_one(
                 {"number": number}, {"$set": {"status": "main"}})
             res.message("You can choose from one of the options below: "
-                        "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order snacks \n 3️⃣ To know our working hours \n 4️⃣ "
+                        "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order food \n 3️⃣ To know our working hours \n 4️⃣ "
                         "To get our address")
         elif 1 <= option <= 9:
-            cakes = ["Red Velvet Cake", "Dark Forest Cake", "Ice Cream Cake",
-                     "Plum Cake", "Sponge Cake", "Genoise Cake", "Angel Cake", "Carrot Cake", "Fruit Cake"]
+            cakes = ["Veg Biryani", "Chicken Biryani", "Chicken Spec.Biryani",
+                     "Mutton Biryani", "Chicken Wings", "Paneer Biryani", "Chicken Curry", "Mutton Curry", "Leg piece"]
             selected = cakes[option - 1]
             users.update_one({"number": number}, {"$set": {"status": "address"}})
             users.update_one({"number": number}, {"$set": {"item": selected}})
@@ -76,9 +77,14 @@ def reply():
         orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
         users.update_one(
             {"number": number}, {"$set": {"status": "ordered"}})
+        users.update_one(
+            {"number": number}, {"$set": {"status": "main"}})
+        res.message("You can choose from one of the options below: "
+                    "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order food \n 3️⃣ To know our working hours \n 4️⃣ "
+                    "To get our address")
     elif user["status"] == "ordered":
         res.message("Hi, thanks for contacting again.\nYou can choose from one of the options below: "
-                    "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order snacks \n 3️⃣ To know our *working hours \n 4️⃣ "
+                    "\n\nType\n\n 1️⃣ To contact us \n 2️⃣ To order food \n 3️⃣ To know our working hours \n 4️⃣ "
                     "To get our address")
         users.update_one({"number": number}, {"$set": {"status": "main"}})
     users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
@@ -87,3 +93,5 @@ def reply():
 
 if __name__ == "__main__":
     app.run()
+
+
